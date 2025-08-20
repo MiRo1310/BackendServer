4a7ded4fa6703b6edf1d7cc277c60521190fbfcb
MiRo1310 <michael.roling@gmx.de>
2025-08-20 16:31:32 +0200

    Update cors pilicy

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173", "http://192.168.1.10")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });

eaf24bdfd30d5199496f0af6dac354dad140aa4c
MiRo1310 <michael.roling@gmx.de>
2025-09-06 12:12:27 +0200

    FEAT: #11 Add query for finance sort by month

---
+++ Models/Finance/TravelCostSortPerMonthDto.cs
@@ -0,0 +1,18 @@
+﻿namespace BackendServer.Models.Finance;
+
+public class TravelCostSortPerMonthDto
+{
+    public required  IQueryable<TravelCost> M1 { get; set; } 
+    public required IQueryable<TravelCost> M2 { get; set; }
+    public required IQueryable<TravelCost> M3 { get; set; }
+    public required IQueryable<TravelCost> M4 { get; set; }
+    public required IQueryable<TravelCost> M5 { get; set; }
+    public required IQueryable<TravelCost> M6 { get; set; }
+    public required IQueryable<TravelCost> M7 { get; set; }
+    public required IQueryable<TravelCost> M8 { get; set; }
+    public required IQueryable<TravelCost> M9 { get; set; }
+    public required IQueryable<TravelCost> M10 { get; set; }
+    public required IQueryable<TravelCost> M11 { get; set; }
+    public required IQueryable<TravelCost> M12 { get; set; }
+    
+}
--- Types/Finance/TravelCostQuery.cs
+++ Types/Finance/TravelCostQuery.cs
@@ -12,6 +12,36 @@ public static class TravelCostQuery
     [UseFiltering]
     public static IQueryable<TravelCost> TravelCost(FinanceDbContext dbContext)
     {
-        return  dbContext.TravelCost.Include(cost => cost.Address );
+        return  dbContext.TravelCost
+            .Include(c => c.Address );
+    }
+    
+   public static TravelCostSortPerMonthDto TravelCostSortPerMonth(FinanceDbContext dbContext, int year)
+    {
+
+        var travelCost = dbContext.TravelCost
+            .Include(t=> t.Address)
+            .Where(t=> t.Date != null && t.Date.Value.Year == year);
+        
+        return  new TravelCostSortPerMonthDto
+        {
+            M1 = GetTravelCostByMonth(travelCost, 1),
+            M2 = GetTravelCostByMonth(travelCost,2),
+            M3 = GetTravelCostByMonth(travelCost,3),
+            M4 = GetTravelCostByMonth(travelCost,4),
+            M5 = GetTravelCostByMonth(travelCost,5),
+            M6 = GetTravelCostByMonth(travelCost,6),
+            M7 = GetTravelCostByMonth(travelCost,7),
+            M8 = GetTravelCostByMonth(travelCost,8),
+            M9 = GetTravelCostByMonth(travelCost,9),
+            M10 =GetTravelCostByMonth(travelCost,10),
+            M11 = GetTravelCostByMonth(travelCost,11),
+            M12 = GetTravelCostByMonth(travelCost,12)
+        };
+    }
+   
+    private static IQueryable<TravelCost> GetTravelCostByMonth(IQueryable<TravelCost> costs,  int month)
+    {
+        return costs.Where(c => c.Date != null && c.Date.Value.Month == month).OrderBy(c => c.Date);
     }
 }

5696f499d56d55d204696cc6d59b22ba489ccbf9
MiRo1310 <michael.roling@gmx.de>
2026-03-07 11:48:31 +0100

    FEAT: Remove ProductPosition column and update product-related table structures

---
+++ Migrations/20260307104416_removeProductPosition.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeProductPosition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductPosition",
+                table: "RecipeProducts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Units",
+                type: "varchar(36)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Recipes",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Category",
+                table: "Products",
+                column: "Category");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductCategories_Category",
+                table: "Products",
+                column: "Category",
+                principalTable: "ProductCategories",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductCategories_Category",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_Category",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Units",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(36)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Recipes",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductPosition",
+                table: "RecipeProducts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
